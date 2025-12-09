d6dbff43d91e9b129cdb813a44d588d17989c8a7
Toan Nguyen <hgiasac@gmail.com>
2025-12-10 00:52:23 +0700

    upgrade opentelemetry sdk v1.39.0

--- provider.go
+++ provider.go
@@ -28,7 +28,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.38.0"
 	traceapi "go.opentelemetry.io/otel/trace"
 )
 
