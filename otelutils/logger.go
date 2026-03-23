428854f5e7f375bc11620f2539075ee594f24f3c
Toan Nguyen <hgiasac@gmail.com>
2026-03-23 09:45:50 +0700

    revert DebugLogger

--- otelutils/logger.go
+++ otelutils/logger.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"slices"
 	"strings"
 )
 
@@ -82,59 +81,4 @@ func GetLogger(ctx context.Context) *slog.Logger {
 	}
 
 	return slog.Default()
-}
-
-// DebugLogger wraps the logger with debug information.
-// It won't print if the log level isn't debug.
-type DebugLogger struct {
-	logger     *slog.Logger
-	attributes []slog.Attr
-	debug      bool
-}
-
-// NewDebugLogger creates a debug logger from context.
-func NewDebugLogger(ctx context.Context, logger *slog.Logger) *DebugLogger {
-	if logger == nil {
-		logger = GetLogger(ctx)
-	}
-
-	return &DebugLogger{
-		logger: logger,
-		debug:  logger.Enabled(ctx, slog.LevelDebug),
-	}
-}
-
-// Grow grows the capacity of the attributes.
-func (dl *DebugLogger) Grow(minCap, maxCap int) {
-	capacity := minCap
-
-	if dl.debug && capacity < maxCap {
-		capacity = maxCap
-	}
-
-	dl.attributes = slices.Grow(dl.attributes, capacity)
-}
-
-// AddAttributes add attributes to the logger.
-func (dl *DebugLogger) AddAttributes(attrs ...slog.Attr) {
-	dl.attributes = append(dl.attributes, attrs...)
-}
-
-// AddDebugAttributes add attributes to the logger.
-// If the log level isn't debug, those attributes will be discarded.
-func (dl *DebugLogger) AddDebugAttributes(attrs ...slog.Attr) {
-	if !dl.debug {
-		return
-	}
-
-	dl.attributes = append(dl.attributes, attrs...)
-}
-
-// Log prints the message log by level.
-func (dl *DebugLogger) Log(level slog.Level, message string) {
-	if dl.debug && level > slog.LevelDebug {
-		return
-	}
-
-	dl.logger.LogAttrs(context.Background(), level, message, dl.attributes...)
 }
